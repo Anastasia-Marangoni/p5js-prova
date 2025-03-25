01f17b057274d7269f0f21d94fd4e2e8ea73c3b6
Anastasia-Marangoni <anastasia00m@gmail.com>
2025-03-25 12:30:39 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,6 +10,10 @@ function draw() {
   rect(0, height - s, s, s);
   rect(width - s, 0, s, s);
   rect(width - s, height - s, s, s);
+
+  let allineamento = width / 2 - 100;
+  text("Anastasia", allineamento, height / 2);
+  textSize(50);
 }
 
 function windowResized() {
